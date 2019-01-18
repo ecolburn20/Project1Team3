2e6d981caf78ecbca52f6945e7c5bba3baad359c
White <James.White@gartner.com>
2019-01-17 20:58:51 -0500

    Initial Push

---
+++ config.py
@@ -0,0 +1,10 @@
+weather_api_key="1490dbe61b03c9fe26248be3e74c7205"
+gkey="AIzaSyD4A1ffKD_pF9V_PTGLlkdjTEHMq2f5UxI"
+census_api_key = "85ac64b6b5a9c0901b00329d1ef41f0c53ccfc98"
+
+
+#Twitter API Keys
+consumer_key="561iTr6WfHqmYMXBiTZmhEwhf"
+consumer_secret="fe0VcnbHNL6UCApALIdL4UZT3W3T5dH8PjFMNDiUkgoHcTjgFE"
+access_token = "1083385740464132097-XzQX5HSfhe3M40uKpabNkNP8OauGuB"
+access_token_secret = "3MeHke2AAEAbyoxMVjjC4P0WnoUSFHVRzimN2g4gN4jpt"
